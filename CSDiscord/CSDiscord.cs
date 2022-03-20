1649e98f34182506f8384920b11161515f3226b1
Swastik2442 <kul.swas321tik@gmail.com>
2022-03-20 12:43:59 +0530

    Add project files.

---
+++ CSDiscord/CSDiscord.cs
@@ -0,0 +1,20 @@
+﻿using ICities;
+using UnityEngine;
+
+namespace CSDiscord
+{
+
+    public class CSDiscord : IUserMod
+    {
+
+        public string Name
+        {
+            get { return "Cities: Skylines Discord Presence"; }
+        }
+
+        public string Description
+        {
+            get { return "This mod enable Discord Rich Presence for Cities: SKylines."; }
+        }
+    }
+}
